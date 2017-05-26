fc42175fa717db948e1fe7312fd2dd0dbedb1051
nattatorn.dev <nattatorn.dev@gmail.com>
2017-05-26 23:31:35 +0700

    🐎 juneing human felling

--- background.js
+++ background.js
@@ -6,46 +6,44 @@ var reloadState = {
 
 function updateTabIcon(id) {
   if (reloadState.active) {
-    chrome.browserAction.setIcon({path: "cat_on.png", tabId: id});
+    chrome.browserAction.setIcon({ path: 'cat_on.png', tabId: id })
   } else {
-    chrome.browserAction.setIcon({path: "cat_off.png", tabId: id});
+    chrome.browserAction.setIcon({ path: 'cat_off.png', tabId: id })
   }
 }
 
 //When extension is clicked, toggle the state of the extenstion
 chrome.browserAction.onClicked.addListener(function(tab) {
-  console.log("Simple Refresh");
+  console.log('Simple Refresh')
 
   //Select the tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    var activeTab = tabs[0]
     //Flip the state
-    reloadState.active = !reloadState.active;
+    reloadState.active = !reloadState.active
     //Send the updated state to the page
-    chrome.tabs.sendMessage(activeTab.id, reloadState);
+    chrome.tabs.sendMessage(activeTab.id, reloadState)
 
     //Update icon
-    updateTabIcon(activeTab.id);
-  });
-});
+    updateTabIcon(activeTab.id)
+  })
+})
 
-chrome.tabs.onUpdated.addListener(function () {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+chrome.tabs.onUpdated.addListener(function() {
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     //Grab the active tab
-    var activeTab = tabs[0];
+    var activeTab = tabs[0]
 
     //Set icon according to state
-    updateTabIcon(activeTab.id);
-  });
-});
+    updateTabIcon(activeTab.id)
+  })
+})
 
 //Listen for requests from page for state
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    //If request is "getState"
-    if (request.getState) {
-      //Send state to page
-      sendResponse(reloadState);
-    }
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  //If request is "getState"
+  if (request.getState) {
+    //Send state to page
+    sendResponse(reloadState)
   }
-);
+})
--- content.js
+++ content.js
@@ -1,26 +1,38 @@
-var reloadState, currentTimeout;
+var reloadState, currentTimeout
 
 function handleTimeout() {
-  if(!document.querySelector("button#submit-order[disabled='disabled']")) {
+  if (!document.querySelector("button#submit-order[disabled='disabled']")) {
     document.getElementById('submit-order').click()
   } else {
     var selectScriptIndex = 0
     for (i = 0; i < document.getElementsByTagName('script').length; i++) {
-        if( document.getElementsByTagName('script')[i].outerHTML.indexOf('window.gon') !== -1) {
-    		selectScriptIndex = i
-    	}
+      if (
+        document
+          .getElementsByTagName('script')[i]
+          .outerHTML.indexOf('window.gon') !== -1
+      ) {
+        selectScriptIndex = i
+      }
     }
 
-    var selectorWindowGon = document.querySelectorAll('script')[selectScriptIndex].outerHTML.split(';gon.')
+    var selectorWindowGon = document
+      .querySelectorAll('script')[selectScriptIndex]
+      .outerHTML.split(';gon.')
     var inject = selectorWindowGon.reduce(function(p, c, k) {
-      return Object.assign(p, {[c.substr(0, c.indexOf('='))]: c.substr(c.indexOf('=') + 1, c.length)})
+      return Object.assign(p, {
+        [c.substr(0, c.indexOf('='))]: c.substr(c.indexOf('=') + 1, c.length)
+      })
     }, {})
-
-    var localStorageKey = 'events:'+inject.event_id+':ticket_types:pending_orders'
-    var localStorageValue = JSON.stringify(Object.keys(JSON.parse(inject.ticket_types_min)).reduce(function(p, c, k) {
-      return k === 0 ? Object.assign(p, {[c]: 1}) : Object.assign(p, {[c]: 0})
-    }, {}))
-
+    var ticketAvailable = JSON.parse(inject.ticket_types_min)
+    var localStorageKey =
+      'events:' + inject.event_id + ':ticket_types:pending_orders'
+    var localStorageValue = JSON.stringify(
+      Object.keys(ticketAvailable).reduce(function(p, c, k) {
+        return k === 0
+          ? Object.assign(p, { [c]: 1 })
+          : Object.assign(p, { [c]: ticketAvailable[c] })
+      }, {})
+    )
     localStorage.setItem(localStorageKey, localStorageValue)
     document.location.reload()
   }
@@ -29,24 +41,24 @@ function handleTimeout() {
 //Set a timeout or remove timeout based on state
 function toggleTimeout() {
   if (reloadState.active) {
-    console.log("Refresh in " + (reloadState.time / 1000) + " seconds");
-    currentTimeout = window.setTimeout(handleTimeout, reloadState.time);
+    console.log('Refresh in ' + reloadState.time / 1000 + ' seconds')
+    currentTimeout = window.setTimeout(handleTimeout, reloadState.time)
   } else {
-    console.log("Refresh cancelled");
-    window.clearTimeout(currentTimeout);
+    console.log('Refresh cancelled')
+    window.clearTimeout(currentTimeout)
   }
 }
 
 //Handler for updating page state and calling toggleTimeout
 function handleMessage(request) {
-  reloadState = request;
-  toggleTimeout();
+  reloadState = request
+  toggleTimeout()
 }
 
 //Listen for extension state to change
-chrome.runtime.onMessage.addListener(handleMessage);
+chrome.runtime.onMessage.addListener(handleMessage)
 
 //Ask for state on load or reload
-chrome.runtime.sendMessage({getState: true}, function(response) {
-  handleMessage(response);
-});
+chrome.runtime.sendMessage({ getState: true }, function(response) {
+  handleMessage(response)
+})
